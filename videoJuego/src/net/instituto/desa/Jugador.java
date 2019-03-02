a3b988866c1efd0f13377be522480715e1d3bb9e
DCP <DCP@10.0.2.15>
2019-03-02 19:54:05 +0100

    Refactorizado para el ejercicio

--- videoJuego/src/net/instituto/desa/Jugador.java
+++ videoJuego/src/net/instituto/desa/Jugador.java
@@ -18,5 +18,23 @@ public class Jugador {
 	public void setX(int x) {
 		this.x = x;
 	}
+	
+	public void derecha(){
+		this.setX(this.getX() + 1);
+		}
+	
+	public void izquierda(){
+		this.setX(this.getX() - 1);
+		}
+	
+	public void arriba(){
+		this.setY(this.getY() - 1);
+		}
+	
+	public void abajo(){
+		this.setY(this.getY() + 1);
+		}
+	
+	
 
 }
